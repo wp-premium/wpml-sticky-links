c4782604011aa09d63726d0c0c2b49a72b646e4e
OnTheGoSystems <info@onthegosystems.com>
2020-02-27 13:24:20 +0100

    1.5.1

--- plugin.php
+++ plugin.php
@@ -2,16 +2,16 @@
 /*
 Plugin Name: WPML Sticky Links
 Plugin URI: https://wpml.org/
-Description: Prevents internal links from ever breaking | <a href="https://wpml.org">Documentation</a> | <a href="https://wpml.org/version/sticky-links-1-5-0/">WPML Sticky Links 1.5.0 release notes</a>
+Description: Prevents internal links from ever breaking | <a href="https://wpml.org">Documentation</a> | <a href="https://wpml.org/version/sticky-links-1-5-1/">WPML Sticky Links 1.5.1 release notes</a>
 Author: OnTheGoSystems
 Author URI: http://www.onthegosystems.com/
-Version: 1.5.0
+Version: 1.5.1
 Plugin Slug: wpml-sticky-links
 */
 
 if(defined('WPML_STICKY_LINKS_VERSION')) return;
 
-define('WPML_STICKY_LINKS_VERSION', '1.5.0');
+define('WPML_STICKY_LINKS_VERSION', '1.5.1');
 define('WPML_STICKY_LINKS_PATH', dirname(__FILE__));
 
 require WPML_STICKY_LINKS_PATH . '/inc/constants.php';
